862f18ab1fec9304bf7f213ca2c21e2bfde10d59
Alessandro Portes <alessandro.portes@gmail.com>
2019-07-04 16:21:49 -0300

    Aula Produtos VS Categorias

--- site.php
+++ site.php
@@ -1,7 +1,8 @@
 <?php
 
 use \Hcode\Page;
-use Hcode\Model\Product;
+use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
 
@@ -12,4 +13,14 @@ $app->get('/', function() {
 		'products'=>Product::checkList($products)
 	]);
 
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new Page();
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+	]);
 });
