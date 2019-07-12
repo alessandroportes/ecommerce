32b3dc2c012be5e47e36b498626b800679ff127e
Alessandro Portes <alessandro.portes@gmail.com>
2019-07-12 15:56:48 -0300

    Aula Cart

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,35 @@ class User extends Model
     const SECRET = "HcodePhp7_Secret";
 	const SECRET_IV = "HcodePhp7_Secret_IV";
 
+    public static function getFromSession()
+    {
+        $user = new User();
+        if (isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0) {
+            $user->setData($_SESSION[User::SESSION]);
+       }
+       return $user;
+    }
+
+    public static function checkLogin($inadmin = true) //Checa o login do site todo e valida se pode acessar a parte administrativa
+    {
+        if (
+            !isset($_SESSION[User::SESSION]) || 
+            !$_SESSION[User::SESSION] || 
+            !(int)$_SESSION[User::SESSION]["iduser"] > 0
+        ){
+            //Não esta logado
+            return false;
+        } else {
+            if ($inadmin === true && $_SESSION[User::SESSION]['inadmin'] === true) {
+                return true;
+            } else if($inadmin === false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
     public static function login($login, $password)
     {
         
@@ -38,10 +67,7 @@ class User extends Model
     public static function verifyLogin($inadmin = true)
     {
 
-        if (!isset($_SESSION[User::SESSION]) || 
-            !$_SESSION[User::SESSION] || 
-            !(int)$_SESSION[User::SESSION]["iduser"] > 0 || 
-            (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin)
+        if (User::checkLogin($inadmin))
         {
             header("Location: /admin/login");
             exit;
@@ -199,5 +225,5 @@ class User extends Model
 		return password_hash($password, PASSWORD_DEFAULT, [
 			'cost'=>12
 		]);
-	}   
+    }
 }
